8c9f2ff87ca4e2282a5a5acc29b9f4fc752f2514
Yariel Gordillo <42778527+yarielg@users.noreply.github.com>
2019-06-30 15:10:24 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -19,7 +19,7 @@ foreach ($books as $book) {
 
 
 //option 2 old school
-global $wpdb;
+/*global $wpdb;
 $wpdb->query( "DELETE FROM wp_posts WHERE post_type='book'" );
 $wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
-$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );*/
